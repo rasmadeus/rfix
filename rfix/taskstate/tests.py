4b04321d8870a13dbe542b261a3a8a6d480a74b2
K. Kulikov <rasmadeus@gmail.com>
2016-07-16 17:15:14 +0300

    - Added test for TaskState.

--- rfix/taskstate/tests.py
+++ rfix/taskstate/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rfix.taskstate.models import TaskState
 
-# Create your tests here.
+
+class TaskStateTest(TestCase):
+    def setUp(self):
+        self._objects = [['closed', 'closed project'], ['opened', 'opened task']]
+
+        for object in self._objects:
+            TaskState.objects.create(name=object[0], description=object[1])
+
+
+    def test_create(self):
+        for object in self._objects:
+            task_state = TaskState.objects.get(name=object[0])
+            self.assertEqual(task_state.name, object[0])
+            self.assertEqual(task_state.description, object[1])
