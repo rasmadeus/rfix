3544dca8ab6e85156d6f67aa265a0ea38564e51d
K. Kulikov <rasmadeus@gmail.com>
2016-07-31 17:02:10 +0300

    - Repaired State test.

--- rfix/task/tests.py
+++ rfix/task/tests.py
@@ -1,17 +1,19 @@
 from django.test import TestCase
-from rfix.taskstate.models import State
+from rfix.task import models
 
 
 class StateTest(TestCase):
     def setUp(self):
-        self._objects = [['closed', 'closed project'], ['opened', 'opened task']]
+        self._objects = [
+            ['closed', 'closed project'],
+            ['opened', 'opened task']
+        ]
 
         for object in self._objects:
-            TaskState.objects.create(name=object[0], description=object[1])
-
+            models.State.objects.create(name=object[0], description=object[1])
 
     def test_create(self):
         for object in self._objects:
-            task_state = TaskState.objects.get(name=object[0])
+            task_state = models.State.objects.get(name=object[0])
             self.assertEqual(task_state.name, object[0])
             self.assertEqual(task_state.description, object[1])
