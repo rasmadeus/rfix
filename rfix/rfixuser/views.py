7ef4367200b5715a043d84d90fed5c41ff921ea4
K. Kulikov <rasmadeus@gmail.com>
2016-08-06 21:58:26 +0300

    - Add login required to user view.

--- rfix/rfixuser/views.py
+++ rfix/rfixuser/views.py
@@ -1,7 +1,13 @@
 from django.views.generic.detail import DetailView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from rfix.rfixuser import models
 
 
 class RfixUserDetail(DetailView):
     model = models.RfixUser
     context_object_name = 'rfixuser'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(RfixUserDetail, self).dispatch(*args, **kwargs)
