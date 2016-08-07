c0af7fd3ed787529e3b203d8232b2d0d63c29d59
K. Kulikov <rasmadeus@gmail.com>
2016-08-07 14:37:25 +0300

    - Fix Users tests.

--- rfix/rfixuser/tests.py
+++ rfix/rfixuser/tests.py
@@ -2,27 +2,27 @@ from django.test import TestCase
 from django.test import RequestFactory
 from django.test import Client
 from rfix.rfixuser import models
-from rfix.rfixuser import views
 
 
 class RfixUserViewTest(TestCase):
     def setUp(self):
         self._slug = 'user'
+        self._user_password = 'password'
         self._user = models.RfixUser.objects.create(
             username='user',
             email='user@user.com',
-            password='password',
             slug=self._slug,
             photo='user.png'
         )
+        self._user.set_password('password')
+        self._user.save()
         self._factory = RequestFactory()
         self._client = Client()
 
-    def test_detail(self):
-        req = self._factory.get('/users/')
-        res = views.RfixUserDetail.as_view()(req, slug=self._slug)
-        self.assertEqual(res.status_code, 200)
-
     def test_detail_url(self):
-        res = self._client.get('/users/{slug}/'.format(slug=self._slug))
+        self._client.login(
+            username=self._user.username,
+            password=self._user_password
+        )
+        res = self._client.get(self._user.get_absolute_url())
         self.assertEqual(res.status_code, 200)
