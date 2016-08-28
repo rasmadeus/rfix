08708a6c54effa1665215414454127f46e8cbe64
K. Kulikov <rasmadeus@gmail.com>
2016-08-28 22:09:22 +0300

    - Start making a User template.

--- rfix/rfixuser/views.py
+++ rfix/rfixuser/views.py
@@ -1,7 +1,20 @@
 from django.views.generic.detail import DetailView
+from django.utils.translation import ugettext_lazy as tr
 from rfix.rfixuser import models
+from rfix.page.models import Page
 
 
 class RfixUserDetail(DetailView):
     model = models.RfixUser
     context_object_name = 'rfixuser'
+
+    def get_context_data(self, **kwargs):
+        context = super(RfixUserDetail, self).get_context_data(**kwargs)
+        context['page'] = Page.guest_default()
+        context['email'] = 'rasmadeus@gmail.com'
+        context['links'] = (
+            {'name': tr('Support'), 'href': '/support'},
+            {'name': tr('Login'), 'href': '/users/login'}
+        )
+        return context
+
