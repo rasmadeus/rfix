75964286c93dde44c60a0494d1d2fa2eb4d23411
K. Kulikov <rasmadeus@gmail.com>
2016-10-02 16:28:37 +0300

    - Add menu to pages with projects.

--- rfix/views.py
+++ rfix/views.py
@@ -2,11 +2,20 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy as tr
+from rfix.project.models import Project
 
 
 def _links_auth(req):
     return {
         'links': (
+            {
+                'name': tr('Projects'),
+                'href': '#',
+                'sublinks': (
+                    {'name': project.name, 'href': project.get_absolute_url()}
+                    for project in Project.objects.all()
+                ),
+            },
             {'name': tr('Support'), 'href': '/support'},
             {'name': tr('Login'), 'href': '/users/login'},
         )
@@ -31,7 +40,8 @@ def _guest(req):
 
 
 def links(req):
-    return _links_auth(req) if req.user.is_authenticated() else _links_guest(req)
+    has_req_user = req.user.is_authenticated
+    return _links_auth(req) if has_req_user else _links_guest(req)
 
 
 def index(req):
