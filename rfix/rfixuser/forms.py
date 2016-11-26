595205715c93316c85c0d1734623f62ff909f0ff
K. Kulikov <rasmadeus@gmail.com>
2016-11-26 15:37:50 +0300

    - Add login view.

--- rfix/rfixuser/forms.py
+++ rfix/rfixuser/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from rfix.rfixuser.models import RfixUser
+from django.contrib.auth import authenticate
+from django.utils.translation import ugettext_lazy as tr
 
 
-class Login(forms.ModelForm):
-    class Meta:
-        model = RfixUser
-        fields = ['username', 'password']
+class Login(forms.Form):
+    username = forms.CharField(label=tr('Login'))
+    password = forms.CharField(label=tr('Password'), widget=forms.PasswordInput)
+
+    def login(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        return authenticate(username=username, password=password)
+
+    def add_login_error(self):
+        self.add_error('password', tr('Your password or login is invalid'))
--- rfix/rfixuser/views.py
+++ rfix/rfixuser/views.py
@@ -4,7 +4,8 @@ from rfix.rfixuser import models
 from rfix.task.models import Task
 from rfix.rfixuser import forms
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.contrib import auth
 
 
 class RfixUserDetail(DetailView):
@@ -38,7 +39,13 @@ def login(req):
     if req.method == 'POST':
         form = forms.Login(req.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/')
+            user = form.login()
+            if user is None:
+                form.add_login_error()
+            else:
+                auth.login(req, user)
+                return redirect('/')
     else:
         form = forms.Login()
+
     return render(req, 'rfixuser/login.html', {'form': form})
